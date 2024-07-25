e8e03754526907af81e85317a854abf064807162
Marco Antônio <marco.netto@mjv.com.br>
2024-07-25 15:50:27 -0300

    feat: :sparkles: adicionando integração ao banco de dados e deixando 100% cov de testes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from curso_fast.schemas import UserPublic
+
 
 def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
@@ -25,30 +27,59 @@ def test_create_user(client):
     }
 
 
+def test_create_user_com_username_ja_existente_deve_retornar_bad_request(
+    client, user
+):
+    response = client.post(
+        '/users',
+        json={
+            'username': 'Teste',
+            'password': 'pass',
+            'email': 'outro_mail@test.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Username already exists'}
+
+
+def test_create_user_com_email_ja_existente_deve_retornar_bad_request(
+    client, user
+):
+    response = client.post(
+        '/users',
+        json={
+            'username': 'outro_username',
+            'password': 'pass',
+            'email': 'teste@test.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Email already registered'}
+
+
 def test_read_users(client):
     response = client.get('/users')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'id': 1,
-                'username': 'test_username',
-                'email': 'test@test.com',
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_read_user(client):
+def test_read_users_with_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_read_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
     response = client.get('/users/1')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'id': 1,
-        'username': 'test_username',
-        'email': 'test@test.com',
-    }
+    assert response.json() == user_schema
 
 
 def test_read_user_com_id_invalido_deve_retornar_not_found(client):
@@ -58,7 +89,7 @@ def test_read_user_com_id_invalido_deve_retornar_not_found(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -90,7 +121,7 @@ def test_update_user_com_id_invalido_deve_retornar_not_found(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
